6918bd528ee952bec651e6fea4f6fe1dd0e5d9dd
Rulo180 <mvallesacc@gmail.com>
2018-09-25 12:06:25 -0300

    - Add list of events in events template. - Add get_user_event method in view.py - Refactor EventView template in urls.
    Update views.py
    
    Update views.py

--- documentsManager/urls.py
+++ documentsManager/urls.py
@@ -4,7 +4,7 @@ from post_registration.views import HomeView
 from .views import EventsView, DocFormView
 
 urlpatterns = [
-    url(r'events/$', EventsView.as_view(template_name='events.html'), name='events'),
     url(r'docs_form/$', DocFormView.as_view(template_name='doc_form.html'), name='doc_form'),
-    url(r'^$', HomeView.as_view(template_name='index.html'), name='index'),
+    url(r'events/$', EventsView.as_view(), name='events'),
+    url(r'^$', HomeView.as_view(), name='index'),
 ]
