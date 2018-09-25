ebb9e426812f3e282985c742ce6a6afe022fa02c
Rulo180 <mvallesacc@gmail.com>
2018-09-25 12:06:25 -0300

    - Add list of events in events template. - Add get_user_event method in view.py - Refactor EventView template in urls.
    Update views.py
    
    Update views.py

--- documentsManager/views.py
+++ documentsManager/views.py
@@ -4,6 +4,10 @@ from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
 from eventbrite import Eventbrite
 from social_django.models import UserSocialAuth
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import datetime
 
 
 @method_decorator(login_required, name='dispatch')
@@ -15,24 +19,35 @@ class EventsView(TemplateView, LoginRequiredMixin):
         context['user'] = self.request.user
         token = get_auth_token(self.request.user)
         eventbrite = Eventbrite(token)
-        context['events'] = eventbrite.get('/users/me/events/')['events']
+        context['events'] = self.get_user_events(eventbrite)
         return context
 
+    def get_user_events(self, eventbrite):
+        events = []
+        api_events = eventbrite.get('/users/me/events/')['events']
+        for event in api_events:
+            view_event = {
+                'id': event['id'],
+                'name': event['name']['text'],
+                'start': datetime.strptime(event['start']['utc'], '%Y-%m-%dT%H:%M:%SZ'), # 2018-11-01T22:00:00Z
+                'end': datetime.strptime(event['end']['utc'], '%Y-%m-%dT%H:%M:%SZ'), # 2018-11-01T22:00:00Z
+            }
+            events.append(view_event)
+        return events
+    
 
-@method_decorator(login_required, name='dispatch')
-class DocFormView(TemplateView, LoginRequiredMixin):
-    template_name = 'doc_form.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(DocFormView, self).get_context_data(**kwargs)
-        context['user'] = self.request.user
-        token = get_auth_token(self.request.user)
-        eventbrite = Eventbrite(token)
-        pepito = self.request.GET['id']
-        pepe = '/events/{}/'.format(pepito)
-        context['event'] = eventbrite.get(pepe)
+@method_decorator(login_required, name='dispatch')	
+class DocFormView(TemplateView, LoginRequiredMixin):	
+    template_name = 'doc_form.html'	
+    def get_context_data(self, **kwargs):	
+        context = super(DocFormView, self).get_context_data(**kwargs)	
+        context['user'] = self.request.user	
+        token = get_auth_token(self.request.user)	
+        eventbrite = Eventbrite(token)	
+        pepito = self.request.GET['id']	
+        pepe = '/events/{}/'.format(pepito)	
+        context['event'] = eventbrite.get(pepe)	
         return context
-
 
 def get_auth_token(user):
     """
