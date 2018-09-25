1c914542482ba0e11e9aa95b092364bf358157c6
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-09-25 11:45:13 -0300

    added FormDocs only View

--- documentsManager/urls.py
+++ documentsManager/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url, include
-from post_registration.views import HomeView
-from .views import EventsView
+from django.conf.urls import url
 
+from post_registration.views import HomeView
+from .views import EventsView, DocFormView
 
 urlpatterns = [
     url(r'events/$', EventsView.as_view(template_name='events.html'), name='events'),
+    url(r'docs_form/$', DocFormView.as_view(template_name='doc_form.html'), name='doc_form'),
     url(r'^$', HomeView.as_view(template_name='index.html'), name='index'),
 ]
--- documentsManager/views.py
+++ documentsManager/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
 from eventbrite import Eventbrite
 from social_django.models import UserSocialAuth
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 @method_decorator(login_required, name='dispatch')
@@ -18,6 +17,22 @@ class EventsView(TemplateView, LoginRequiredMixin):
         eventbrite = Eventbrite(token)
         context['events'] = eventbrite.get('/users/me/events/')['events']
         return context
+
+
+@method_decorator(login_required, name='dispatch')
+class DocFormView(TemplateView, LoginRequiredMixin):
+    template_name = 'doc_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DocFormView, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+        token = get_auth_token(self.request.user)
+        eventbrite = Eventbrite(token)
+        pepito = self.request.GET['id']
+        pepe = '/events/{}/'.format(pepito)
+        context['event'] = eventbrite.get(pepe)
+        return context
+
 
 def get_auth_token(user):
     """
