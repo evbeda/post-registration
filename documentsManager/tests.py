117177a350f8e246c654fa36e1577ed231d20741
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-09-25 11:45:13 -0300

    added FormDocs only View

--- documentsManager/tests.py
+++ documentsManager/tests.py
@@ -1,3 +1,12 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
-# Create your tests here.
+
+class DocFormTest(TestCase):
+    def test_doc_from_exist(self):
+        view = resolve('/docs_form/')
+        self.assertEqual(view.view_name, 'doc_form')
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEqual(view.view_name, 'index')
