cebfa87c29390c2ea89186ee67bf2305a335c00c
Rulo180 <mvallesacc@gmail.com>
2018-09-25 09:56:09 -0300

    - Fix HomeView import in app url. - Refactor /events url pattern.

--- documentsManager/urls.py
+++ documentsManager/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
-from ../post_registration/views import HomeView
+from post_registration.views import HomeView
 from .views import EventsView
 
 
 urlpatterns = [
+    url(r'events/$', EventsView.as_view(template_name='events.html'), name='events'),
     url(r'^$', HomeView.as_view(template_name='index.html'), name='index'),
-    url(r'events/', EventsView.as_view(template_name='events.html', name='events'))
 ]
