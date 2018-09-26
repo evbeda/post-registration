220d689cd3f824850c26b417612565eaa3db6d3b
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-09-26 15:20:29 -0300

    Added more test for views and auth token

---
+++ README.md
@@ -0,0 +1,6 @@
+# Post Registration
+
+EDA 2.0 - Post Registration
+---
+[![Coverage Status](https://coveralls.io/repos/github/evbeda/post-registration/badge.svg?branch=master)](https://coveralls.io/github/evbeda/post-registration?branch=master)
+[![Build Status](https://travis-ci.org/evbeda/post-registration.svg?branch=master)](https://travis-ci.org/evbeda/post-registration)
--- documentsManager/views.py
+++ documentsManager/views.py
@@ -1,13 +1,11 @@
+from datetime import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
 from eventbrite import Eventbrite
 from social_django.models import UserSocialAuth
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.contrib.auth.mixins import LoginRequiredMixin
-from datetime import datetime
 
 
 @method_decorator(login_required, name='dispatch')
@@ -63,5 +61,6 @@ def get_auth_token(user):
             provider='eventbrite'
         ).access_token
     except UserSocialAuth.DoesNotExist:
-        print('UserSocialAuth does not exists!')
+        error_msg = 'UserSocialAuth does not exists!'
+        raise Exception(error_msg)
     return token
