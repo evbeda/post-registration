d716ab4a42dab1012e06352326a01d6864f02e27
Rulo180 <mvallesacc@gmail.com>
2018-10-01 10:45:09 -0300

    Minor refactor to Models and docs template - Add table_name to all models. - Minor visual improvements to docs template.

--- documentsManager/models.py
+++ documentsManager/models.py
@@ -8,10 +8,16 @@ class Event(models.Model):
     init_submission = models.DateField(default=datetime.date.today)
     end_submission = models.DateField(blank=True, null=True)
 
+    class Meta:
+        db_table = 'Event'
+
 
 class FileType(models.Model):
     name = models.CharField(max_length=5)
     description = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        db_table = 'FileType'
 
     def __str__(self):
         return self.name
@@ -22,3 +28,6 @@ class Doc(models.Model):
     file_type = models.ForeignKey(FileType)
     is_optional = models.BooleanField(default=True)
     event = models.ForeignKey(Event, blank=True)
+
+    class Meta:
+        db_table = 'Doc'
