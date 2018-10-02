39905d13df7687bb2ef1e28126955fdc4c46d33f
rominic <romina.avila.luque@outlook.com>
2018-10-02 14:23:16 -0300

    Refactor Views - Extract logic for fuction: filter api events, get events from API - Add test for View

--- documentsManager/urls.py
+++ documentsManager/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
-from post_registration.views import HomeView
-from .views import EventsView, DocFormView , DocsView
+from .views import EventsView, DocFormView, DocsView, HomeView
 
 urlpatterns = [
     url(r'doc_form/(?P<event_id>\d+)/$', DocFormView.as_view(), name='doc_form'),
-    url(r'docs/(?P<event_id>\d+)/$', DocsView.as_view(template_name='docs.html'), name='docs'),
+    url(r'docs/(?P<event_id>\d+)/$', DocsView.as_view(), name='docs'),
     url(r'events/$', EventsView.as_view(), name='events'),
     url(r'events/(?P<eb_event_id>\d+)/$', EventsView.as_view(), name='events'),
-    url(r'^$', HomeView.as_view(), name='index'),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
