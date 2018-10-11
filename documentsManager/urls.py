f02d36d9b443599df2b36005dd4a057e1780512f
Rulo180 <mvallesacc@gmail.com>
2018-10-11 15:33:43 -0300

    Delete Docs feature - Add FileDocDelete and TextDocDelete views. - Add urls to delete file and text docs. - Add doc_confirm_delete template. - Refactor doc_update template to be reused in both UpdateViews.

--- documentsManager/urls.py
+++ documentsManager/urls.py
@@ -7,15 +7,21 @@ from .views import (
     HomeView,
     FileDocUpdate,
     TextDocUpdate,
+    FileDocDelete,
+    TextDocDelete,
     select_event,
 )
 
 urlpatterns = [
     url(r'doc_form/(?P<event_id>\d+)/$', DocFormView.as_view(), name='doc_form'),
-    url(r'^docs/file/edit/(?P<id>\d+)/$',
+    url(r'^docs/file/edit/(?P<pk>\d+)/$',
         FileDocUpdate.as_view(), name="edit-filedoc"),
-    url(r'^docs/text/edit/(?P<id>\d+)/$',
+    url(r'^docs/text/edit/(?P<pk>\d+)/$',
         TextDocUpdate.as_view(), name="edit-textdoc"),
+    url(r'^docs/file/delete/(?P<pk>\d+)/$',
+        FileDocDelete.as_view(), name="delete-filedoc"),
+    url(r'^docs/text/delete/(?P<pk>\d+)/$',
+        TextDocDelete.as_view(), name="delete-textdoc"),
     url(r'docs/(?P<event_id>\d+)/$', DocsView.as_view(), name='docs'),
     url(r'events/$', EventsView.as_view(), name='events'),
     url(r'events/(?P<eb_event_id>\d+)/$', select_event, name='events'),
