a3823d7b53d3adaa797b5579e8e004089a9e9ce2
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-10-30 16:00:58 -0300

    added dashboard of submissions

--- documentsManager/urls.py
+++ documentsManager/urls.py
@@ -19,35 +19,108 @@ from .views import (
     EvaluatorDelete,
     accept_webhook,
     SubmissionView,
+    SubmissionsList,
 )
 
 urlpatterns = [
-    url(r'landing/(?P<event_id>\d+)/$', LandingView.as_view(), name='landing'),
-    url(r'success/$', SuccessView.as_view(), name='success'),
-    url(r'landing/(?P<event_id>\d+)/success/$',
-        SuccessView.as_view(), name='success'),
-    url(r'doc_form/(?P<event_id>\d+)/$', DocFormView.as_view(), name='doc_form'),
-    url(r'^event/(?P<event_id>\d+)/docs/file/(?P<pk>\d+)/edit/$',
-        FileDocUpdate.as_view(), name="edit-filedoc"),
-    url(r'^event/(?P<event_id>\d+)/evaluator/add/$',
-        EvaluatorCreate.as_view(), name='evaluator_create'),
-    url(r'^event/(?P<event_id>\d+)/evaluator/(?P<pk>\d+)/edit/$',
-        EvaluatorUpdate.as_view(), name='evaluator_update'),
-    url(r'^event/(?P<event_id>\d+)/evaluator/(?P<pk>\d+)/delete/$',
-        EvaluatorDelete.as_view(), name='evaluator_delete'),
-    url(r'^event/(?P<event_id>\d+)/evaluators/$',
-        EvaluatorList.as_view(), name='evaluators'),
-    url(r'^event/(?P<event_id>\d+)/docs/text/(?P<pk>\d+)/edit/$',
-        TextDocUpdate.as_view(), name="edit-textdoc"),
-    url(r'^event/(?P<event_id>\d+)/docs/file/(?P<pk>\d+)/delete/$',
-        FileDocDelete.as_view(), name="delete-filedoc"),
-    url(r'^event/(?P<event_id>\d+)/docs/text/(?P<pk>\d+)/delete/$',
-        TextDocDelete.as_view(), name="delete-textdoc"),
-    url(r'event/(?P<event_id>\d+)/docs/$', DocsView.as_view(), name='docs'),
-    url(r'events/$', EventsView.as_view(), name='events'),
-    url(r'events/(?P<eb_event_id>\d+)/$', select_event, name='events'),
-    url(r'signup/$', SignUpView.as_view(), name='signup'),
-    url(r'event/(?P<event_id>\d+)/submissions/$', SubmissionView.as_view(), name='submission'),
-    url(r'^webhook-point/', accept_webhook, name='accept_webhook'),
-    url(r'^$', HomeView.as_view(), name='home'),
+    url(
+        r'landing/(?P<event_id>\d+)/$',
+        LandingView.as_view(),
+        name='landing'
+    ),
+    url(
+        r'success/$',
+        SuccessView.as_view(),
+        name='success'
+    ),
+    url(
+        r'landing/(?P<event_id>\d+)/success/$',
+        SuccessView.as_view(),
+        name='success'
+    ),
+    url(
+        r'doc_form/(?P<event_id>\d+)/$',
+        DocFormView.as_view(),
+        name='doc_form'
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/docs/file/(?P<pk>\d+)/edit/$',
+        FileDocUpdate.as_view(),
+        name="edit-filedoc"
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/evaluator/add/$',
+        EvaluatorCreate.as_view(),
+        name='evaluator_create'
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/evaluator/(?P<pk>\d+)/edit/$',
+        EvaluatorUpdate.as_view(),
+        name='evaluator_update'
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/evaluator/(?P<pk>\d+)/delete/$',
+        EvaluatorDelete.as_view(),
+        name='evaluator_delete'
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/evaluators/$',
+        EvaluatorList.as_view(),
+        name='evaluators'
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/submissions/$',
+        SubmissionsList.as_view(),
+        name='submissions'
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/docs/text/(?P<pk>\d+)/edit/$',
+        TextDocUpdate.as_view(),
+        name="edit-textdoc"
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/docs/file/(?P<pk>\d+)/delete/$',
+        FileDocDelete.as_view(),
+        name="delete-filedoc"
+    ),
+    url(
+        r'^event/(?P<event_id>\d+)/docs/text/(?P<pk>\d+)/delete/$',
+        TextDocDelete.as_view(),
+        name="delete-textdoc"
+    ),
+    url(
+        r'event/(?P<event_id>\d+)/docs/$',
+        DocsView.as_view(),
+        name='docs'
+    ),
+    url(
+        r'events/$',
+        EventsView.as_view(),
+        name='events'
+    ),
+    url(
+        r'events/(?P<eb_event_id>\d+)/$',
+        select_event,
+        name='events'
+    ),
+    url(
+        r'event/(?P<event_id>\d+)/submissions/$',
+        SubmissionView.as_view(),
+        name='submission'
+    ),
+    url(
+        r'signup/$',
+        SignUpView.as_view(),
+        name='signup'
+    ),
+    url(
+        r'^webhook-point/',
+        accept_webhook,
+        name='accept_webhook'
+    ),
+    url(
+        r'^$',
+        HomeView.as_view(),
+        name='home'
+    ),
 ]
